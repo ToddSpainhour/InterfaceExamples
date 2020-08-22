690f507ff32a92f9534cf5aa5943fd72bedb2a60
Todd Spainhour <toddspainhour@gmail.com>
2020-08-22 13:46:08 -0500

    Add project files.

---
+++ InterfaceExamples/Duck.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterfaceExamples
+{
+    class Duck : IFlyable
+    {
+        public void Fly()
+        {
+            Console.WriteLine("I'm a duck! Let's fly!");
+        }
+    }
+
+
+    // Airplane implements IFlyable or Airplane implements ITakeOff
+    class Airplane : IFlyable, ITakeOff
+    {
+        public void Fly()
+        {
+            Console.WriteLine("I'm a plane! Need to get somewhere quick?");
+        }
+
+        public void TakeOff(string destination)
+        {
+            Console.WriteLine($"Taking off to go to {destination}");
+        }
+    }
+
+
+    // Helicopter implements IFlyable or Helicopter implements ITakeOff
+    class Helicopter :IFlyable, ITakeOff
+    {
+        public void Fly()
+        {
+            Console.WriteLine("Get to the choppa!");
+        }
+
+        public void TakeOff(string destination)
+        {
+            Console.WriteLine($"Taking off to go to {destination}");
+        }
+    }
+
+
+
+
+
+
+    // all start with 'I' followed by what they do
+    // classes that use this interface 'IFlyable' must have this method 'Fly()'
+    // using an interfacei'm obligated to have methods like this'
+    interface IFlyable
+    {
+        void Fly();
+    }
+
+    interface ITakeOff
+    {
+        void TakeOff(string destination);
+    }
+
+
+
+
+
+
+}
+
+
---
+++ InterfaceExamples/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace InterfaceExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Interface Examples");
+
+            var duck = new Duck();
+            var plane = new Airplane();
+            var helicopter = new Helicopter();
+
+            var flyingThings = new List<IFlyable> { duck, plane, helicopter };
+
+            foreach (var thing in flyingThings)
+            {
+                if (thing is ITakeOff takeOff)
+                {
+                    takeOff.TakeOff("Bahamas");
+                }
+
+                thing.Fly();
+            }
+
+
+
+        }
+    }
+}
+
+
+// what's the difference between an abstract class and a interface
